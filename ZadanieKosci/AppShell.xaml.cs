55aec7a7b6395ad9d12445d0020c6f5ce8d32525
DanielChwaluk <chwalukdaniel077@gmail.com>
2024-09-18 13:59:20 +0200

    Dodaj pliki projektów.

---
+++ ZadanieKosci/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace ZadanieKosci
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ ZadanieKosci/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace ZadanieKosci
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
